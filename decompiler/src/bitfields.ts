99b0bb431f0f63c4313d0869fd2ad9b74e10b073
rini <rini@rinici.de>
2025-09-07 00:08:15 -0300

    switch to 4-indentation

--- decompiler/src/bitfields.ts
+++ decompiler/src/bitfields.ts
@@ -1,74 +1,74 @@
 import { Bitfield, type ParsedBitfield } from "./Bitfield.ts";
 
 const functionHeaderFlagFields = {
-  prohibitInvoke: 2,
-  strictMode: 1,
-  hasExceptionHandler: 1,
-  hasDebugInfo: 1,
-  overflowed: 1,
+    prohibitInvoke: 2,
+    strictMode: 1,
+    hasExceptionHandler: 1,
+    hasDebugInfo: 1,
+    overflowed: 1,
 };
 
 export const smallFunctionHeader = new Bitfield({
-  offset: 25,
-  paramCount: 7,
-  bytecodeSizeInBytes: 15,
-  functionName: 17,
-  infoOffset: 25,
-  frameSize: 7,
-  environmentSize: 8,
-  highestReadCacheIndex: 8,
-  highestWriteCacheIndex: 8,
+    offset: 25,
+    paramCount: 7,
+    bytecodeSizeInBytes: 15,
+    functionName: 17,
+    infoOffset: 25,
+    frameSize: 7,
+    environmentSize: 8,
+    highestReadCacheIndex: 8,
+    highestWriteCacheIndex: 8,
 
-  ...functionHeaderFlagFields,
+    ...functionHeaderFlagFields,
 });
 
 export const largeFunctionHeader = new Bitfield({
-  offset: 32,
-  paramCount: 32,
-  bytecodeSizeInBytes: 32,
-  functionName: 32,
-  infoOffset: 32,
-  frameSize: 32,
-  environmentSize: 32,
-  highestReadCacheIndex: 8,
-  highestWriteCacheIndex: 8,
+    offset: 32,
+    paramCount: 32,
+    bytecodeSizeInBytes: 32,
+    functionName: 32,
+    infoOffset: 32,
+    frameSize: 32,
+    environmentSize: 32,
+    highestReadCacheIndex: 8,
+    highestWriteCacheIndex: 8,
 
-  ...functionHeaderFlagFields,
+    ...functionHeaderFlagFields,
 });
 
 export type FunctionHeader = ParsedBitfield<typeof largeFunctionHeader>;
 
 export const stringKind = new Bitfield({
-  count: 31,
-  kind: 1,
+    count: 31,
+    kind: 1,
 });
 
 export type StringKind = ParsedBitfield<typeof stringKind>;
 
 export const identifierHash = new Bitfield({
-  hash: 32,
+    hash: 32,
 });
 
 export type IdentifierHash = ParsedBitfield<typeof identifierHash>;
 
 export const stringTableEntry = new Bitfield({
-  isUtf16: 1,
-  offset: 23,
-  length: 8,
+    isUtf16: 1,
+    offset: 23,
+    length: 8,
 });
 
 export type StringTableEntry = ParsedBitfield<typeof stringTableEntry>;
 
 export const offsetLengthPair = new Bitfield({
-  offset: 32,
-  length: 32,
+    offset: 32,
+    length: 32,
 });
 
 export type OffsetLengthPair = ParsedBitfield<typeof offsetLengthPair>;
 
 export const functionSourceEntry = new Bitfield({
-  functionId: 32,
-  stringId: 32,
+    functionId: 32,
+    stringId: 32,
 });
 
 export type FunctionSourceEntry = ParsedBitfield<typeof functionSourceEntry>;
--- scripts/extract.ts
+++ scripts/extract.ts
@@ -6,27 +6,27 @@ import { basename } from "path";
 $("adb", "wait-for-device");
 
 const packages = $("adb", "shell", "pm path com.discord")
-  .replaceAll(/^package:/gm, "")
-  .split("\n")
-  .filter(name => /base\.apk$|split_config\.(arm.*|x86.*|xxhdpi|en)\.apk$/.test(name));
+    .replaceAll(/^package:/gm, "")
+    .split("\n")
+    .filter(name => /base\.apk$|split_config\.(arm.*|x86.*|xxhdpi|en)\.apk$/.test(name));
 
 for (const pkg of packages) {
-  $("adb", "pull", pkg, `discord/${basename(pkg)}`);
+    $("adb", "pull", pkg, `discord/${basename(pkg)}`);
 }
 
 const ANDROID_BUNDLE = "assets/index.android.bundle";
 
 unzip(
-  readFileSync("discord/base.apk"),
-  { filter: (file) => file.name === ANDROID_BUNDLE },
-  (err, data) => {
-    if (err) throw err;
+    readFileSync("discord/base.apk"),
+    { filter: (file) => file.name === ANDROID_BUNDLE },
+    (err, data) => {
+        if (err) throw err;
 
-    writeFileSync("discord/bundle.hbc", data[ANDROID_BUNDLE]);
-  },
+        writeFileSync("discord/bundle.hbc", data[ANDROID_BUNDLE]);
+    },
 );
 
 function $(exe: string, ...args: string[]) {
-  console.log(`$ ${exe} ${args.join(" ")}`);
-  return execFileSync(exe, args, { encoding: "utf8" });
+    console.log(`$ ${exe} ${args.join(" ")}`);
+    return execFileSync(exe, args, { encoding: "utf8" });
 }

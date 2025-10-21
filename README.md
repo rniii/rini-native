b5a50ff3b9001af3d13306191e7902c53c9f4434
paige <235818692+paigely@users.noreply.github.com>
2025-10-21 14:35:33 -0500

    extract:ios (#6)

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Work in progress bytecode-based Discord patcher
 
 ```
 $ pnpm i
-$ pnpm extract            # Grab Discord's APK using `adb`. USB cable recommended.
+$ pnpm extract:android    # Grab Discord's APK using `adb`. USB cable recommended.
+$ pnpm extract:ios        # Grab Discord's IPA using `ipatool`.
 $ pnpm disasm             # Dump bytecode
 $ less -R bytecode.ansi   # Read using a pager
 ```
--- scripts/extract.ts
+++ scripts/extract.ts
@@ -3,28 +3,39 @@ import { unzip } from "fflate";
 import { readFileSync, writeFileSync } from "fs";
 import { basename } from "path";
 
-if (!process.argv.includes("--no-pull")) {
-    $("adb", "wait-for-device");
+let base;
+let bundle;
 
-    const packages = $("adb", "shell", "pm path com.discord")
-        .replaceAll(/^package:/gm, "")
-        .split("\n")
-        .filter(name => /base\.apk$|split_config\.(arm.*|x86.*|xxhdpi|en)\.apk$/.test(name));
+if (!process.argv.includes("ios")) {
+    base = "discord/base.apk";
+    bundle = "assets/index.android.bundle";
+    if (!process.argv.includes("--no-pull")) {
+        $("adb", "wait-for-device");
 
-    for (const pkg of packages) {
-        $("adb", "pull", pkg, `discord/${basename(pkg)}`);
+        const packages = $("adb", "shell", "pm path com.discord")
+            .replaceAll(/^package:/gm, "")
+            .split("\n")
+            .filter(name => /base\.apk$|split_config\.(arm.*|x86.*|xxhdpi|en)\.apk$/.test(name));
+
+        for (const pkg of packages) {
+            $("adb", "pull", pkg, `discord/${basename(pkg)}`);
+        }
+    }
+} else {
+    base = "discord/base.ipa";
+    bundle = "Payload/Discord.app/main.jsbundle";
+    if (!process.argv.includes("--no-pull")) {
+        $("ipatool", "download", "-b", "com.hammerandchisel.Discord", "-o", base, "--purchase");
     }
 }
 
-const ANDROID_BUNDLE = "assets/index.android.bundle";
-
 unzip(
-    readFileSync("discord/base.apk"),
-    { filter: file => file.name === ANDROID_BUNDLE },
+    readFileSync(base),
+    { filter: file => file.name === bundle },
     (err, data) => {
         if (err) throw err;
 
-        writeFileSync("discord/bundle.hbc", data[ANDROID_BUNDLE]);
+        writeFileSync("discord/bundle.hbc", data[bundle]);
     },
 );
 
